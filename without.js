cd7a15bc8c11308547706a91e1508e253254d26c
paige-clark <paigeclarkillustration@gmail.com>
2022-07-26 23:17:40 +0000

    Added a function that intakes an array and items to remove from it.

---
+++ without.js
@@ -0,0 +1,12 @@
+// REMOVES AN ARRAY OF ITEMS FROM A SOURCE ARRAY AND RETURNS A NEW ARRAY
+const without = (sourceArray, itemsToRemove) => {
+  let result = [];
+  for (let item of sourceArray) {
+    if (!itemsToRemove.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+console.log(without([4, 5, 6, 5, 9, 10], [4, '5']));
