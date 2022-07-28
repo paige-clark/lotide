5607d88ab556ba5d297d484cea43239df6e702ec
paige-clark <paigeclarkillustration@gmail.com>
2022-07-28 20:43:24 +0000

    Added a function that iterates through and array until a condition set in an anon function is met, also made small tweak to assert equal arrays to make the results cleaner.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,21 @@
 // COMPARE TWO ARRAYS FOR EXACT MATCH AND LOGS MESSAGE
 const assertArraysEqual = (arrayOne, arrayTwo) => {
+  const inspect = require('util').inspect;
   // if they are not the same length, false
   if (arrayOne.length !== arrayTwo.length) {
-    console.log(`🔴🔴🔴 Arrays DON'T match: ${arrayOne} !== ${arrayTwo}`);
+    console.log(`🔴🔴🔴 Arrays DON'T match: ${inspect(arrayOne)} !== ${inspect(arrayTwo)}`);
     return false;
   }
 
   // if the index values don't match, false
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
-      console.log(`🔴🔴🔴 Arrays DON'T match: ${arrayOne} !== ${arrayTwo}`);
+      console.log(`🔴🔴🔴 Arrays DON'T match: ${inspect(arrayOne)} !== ${inspect(arrayTwo)}`);
       return false;
     }
   }
 
   // otherwise return true
-  console.log(`🟢🟢🟢 Arrays DO match: ${arrayOne} === ${arrayTwo}`);
+  console.log(`🟢🟢🟢 Arrays DO match: ${inspect(arrayOne)} === ${inspect(arrayTwo)}`);
   return true;
 };
---
+++ takeUntil.js
@@ -0,0 +1,53 @@
+/*
+a function that iterates through an array, collecting items, until it finds a truthy value
+INPUT: function that takes in an array and a callback function
+-
+OUTPUT: a slice of an array with elements taken from the beginning
+*/
+
+// ITERATES THROUGH ARRAY UNTIL RULES OF CALLBACK RETURN TRUE
+const takeUntil = (array, callback) => {
+  let result = [];
+  for (let item of array) {
+    if (callback(item) === false) {
+      result.push(item);
+    } else break;
+  }
+  return result;
+};
+
+// TEST INPUTS
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1); // [ 1, 2, 5, 7, 2 ]
+
+// console.log('---');
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2); // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+// // COMPARE TWO ARRAYS FOR EXACT MATCH AND LOGS MESSAGE
+// const assertArraysEqual = (arrayOne, arrayTwo) => {
+//   const inspect = require('util').inspect;
+//   // if they are not the same length, false
+//   if (arrayOne.length !== arrayTwo.length) {
+//     console.log(`🔴🔴🔴 Arrays DON'T match: ${inspect(arrayOne)} !== ${inspect(arrayTwo)}`);
+//     return false;
+//   }
+
+//   // if the index values don't match, false
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]) {
+//       console.log(`🔴🔴🔴 Arrays DON'T match: ${inspect(arrayOne)} !== ${inspect(arrayTwo)}`);
+//       return false;
+//     }
+//   }
+
+//   // otherwise return true
+//   console.log(`🟢🟢🟢 Arrays DO match: ${inspect(arrayOne)} === ${inspect(arrayTwo)}`);
+//   return true;
+// };
+
+// assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+// assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ])
